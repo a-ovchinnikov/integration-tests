52e885fdc2af2ebc9d2fb27560a0ca156afd0567
Alexey Ovchinnikov <aovchinn@redhat.com>
2025-04-02 13:55:25 -0500

    Simplification of test app

--- src/test_package_cachi2/main.py
+++ src/test_package_cachi2/main.py
@@ -3,18 +3,7 @@
 A simple script for checking pre-installed packages provided by Cachi2
 """
 
-# import io
-# import requests
-# from dockerfile_parse import DockerfileParser
+# This will fail if the package is missing.
+# If this does not fail then we can assume that the package
+# was successfully built and installed.
 from cryptography.fernet import Fernet
-
-
-if __name__ == '__main__':
-    print("As expected")
-    # dfp = DockerfileParser(fileobj=io.BytesIO())
-    # containerfile_url = "https://raw.githubusercontent.com/containerbuildsystem/cachi2/e793f7c1c446994a353769a89c8395a3ed0639bc/Containerfile"
-    # r = requests.get(containerfile_url, allow_redirects=True)
-    # dfp.content = r.content
-
-    # # Print parent image
-    # print(dfp.parent_images[0])
