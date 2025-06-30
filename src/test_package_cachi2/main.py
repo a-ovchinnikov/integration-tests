6144f7a5cf11dced4772769a9d4cd3bc17ccc721
Alexey Ovchinnikov <aovchinn@redhat.com>
2025-06-30 12:00:41 -0500

    Revert "More tests"
    This reverts commit 38f6ee425a920d54bc0c027d9ea5c5d94e4996d7.

--- src/test_package_cachi2/main.py
+++ src/test_package_cachi2/main.py
@@ -6,5 +6,4 @@ A simple script for checking pre-installed packages provided by Cachi2
 # This will fail if the package is missing.
 # If this does not fail then we can assume that the package
 # was successfully built and installed.
-# from cryptography.fernet import Fernet
-import tiktoken
+from cryptography.fernet import Fernet
